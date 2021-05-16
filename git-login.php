50d0623ee7e47a64a9e79737bf1496b0762fcdfc
Beytullah <Beytullah_Yayla@outlook.com>
2021-05-16 15:47:51 +0300

    Create git-login.php
    login sayfasındaki bilgilerin gidecegi ve kontrollerin yapilacagi php sayfasi

---
+++ git-login.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<!-- Responsive -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<!-- Ikon Kutuphanesi -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+	<!-- Font -->
+	<link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans&display=swap" rel="stylesheet">
+	
+	<!-- Css -->
+	<link rel="stylesheet" href="style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+	<!--------------------------------->
+	<title>Web Teknoloji Projesi</title>
+
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="Hakkında.html">Beytullah Yayla</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto" >
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="Hakkında.html">Hakkımda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Özgeçmiş.html">Özgeçmiş</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="İletişim.html">İletişim</a>
+        </li>
+        <li class="nav-item">
+  		<a class="nav-link" href="İlgi alanlarım.html">İlgi Alanlarım</a>
+  	</li>
+        <li class="nav-item">
+        	<div class="dropdown">
+           <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" id="dropdownMenuButton1" role="button" aria-expanded="false">Şehrim</a>
+           <ul class="dropdown-menu" >
+           		<li><a class="dropdown-item" href="Sakarya.html">Sakaryam</a></li>
+   				 
+  				 <li><a class="dropdown-item" href="Sakaryaspor.html">Sakarya Spor</a></li>
+  		</ul>
+  	</li>
+  	<li class="nav-item">
+  		<a class="nav-link active" href="Login.html">Giriş Yap</a>
+  	</li>
+
+  	
+
+  				
+
+  </ul>
+  
+  
+</div>
+
+ 
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+<br>
+	
+	<main>
+
+		<div class="container">
+			<br>
+			<?php 
+
+				include("users.php");
+
+
+				if (($_POST["id"] == $user) and ($_POST["password"] == $pass))
+				{
+			       $_SESSION["login"] = "true";
+			       $_SESSION["user"] = $user;
+			       $_SESSION["pass"] = $pass;
+					
+			       echo("<strong>Giriş Başarılı !!</strong> <br>");
+			       echo("<strong>Hoşgeldiniz</strong>");
+			
+				}
+			
+				else 
+				{
+			            echo "<strong>Kullancı Adı veya Şifre Yanlış.</strong><br>";
+			            echo "<strong>!!!TEKRAR DENEYİN!!!</strong>";
+			            header("Refresh: 1; url=login.php");
+			    }
+			
+			?>
+			
+		</div>
+
+	</main>
+
+
+	<footer class="py-2 bg-dark text-light text-center fixed-bottom">
+		Web-Teknolojileri-Projesi © Beytullah Yayla 2021
+	</footer>
+
+	<!-- BOOTSTRAP -->
+	
+
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
+
