4c4f32f9c006122a7e390baeb3a9608df2e11253
Beytullah <Beytullah_Yayla@outlook.com>
2021-05-16 15:44:19 +0300

    git.html

--- login.php
+++ login.php
@@ -5,30 +5,31 @@
 
 	<meta charset="utf-8">
 
-	
+	<!-- Responsive -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
-	
+	<!-- Ikon Kutuphanesi -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 
-	
+	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-	
+	<!-- Font -->
 	<link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans&display=swap" rel="stylesheet">
 	
-
+	<!-- Css -->
+	<link rel="stylesheet" href="style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="style.css">
 
-	
+	<!--------------------------------->
 	<title>Web Teknoloji Projesi</title>
 
 </head>
 
 <body>
 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="Hakkında.html">Beytullah Yayla</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -58,7 +59,7 @@
   		</ul>
   	</li>
   	<li class="nav-item">
-  		<a class="nav-link" href="Login.html">Giriş Yap</a>
+  		<a class="nav-link active" href="Login.html">Giriş Yap</a>
   	</li>
 
   	
@@ -79,92 +80,50 @@
 <br>
 <br>
 	
-	
-	<main>
+	<main role="main">
 		<div class="jumbotron  bg-light text-dark text-center ">
 		
-			<h2 class="display-4 text-center">Gönderme İşlemi Başarılı</h2>
+			<h2 class="display-4 ">Giriş Ekranı</h2>
+			
 		
-		</div>
+	</div>
 
 		<div class="container">
 			
-			<div class="row">
+			<form action="git-Login.php" method="POST">
 
+				<div class="form-group">
+					<label for="id">Kullanici Adi</label>
+					<input type="text" name="id" class="form-control" placeholder="Kullanici Adi" required="required">
+				</div>
 
+				<div class="form-group">
+					<label for="password">Şifre</label>
+					<input type="password" name="password" class="form-control" placeholder="Şifre '123'" required="required">
+				</div>
 
+				<button class="btn btn btn-dark" type="submit">Giriş</button>
 
-				<script type="text/javascript">
+				<br><br><br>
 
-					function getParameterByName(name, url) 
-					{
-		            	if (!url) url = window.location.href;
-    		       		name = name.replace(/[\[\]]/g, '\\$&');
-        		    	var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-            		    	results = regex.exec(url);
-           				if (!results) return null;
-           				if (!results[2]) return '';
-           				return decodeURIComponent(results[2].replace(/\+/g, ' '));
-        			}
-
-  				    var name = getParameterByName('Ad');
-  				    var email = getParameterByName('email');
-  				    var message = getParameterByName('message');
-
-				</script>
-
-				<table class="table table-hover">
-						
-
-						<tbody>
-							<tr>
-								<td>İsminiz</td>
-								<td>
-									<script>document.write(name);
-									</script>
-								</td>
-							</tr>
-	
-							<tr>
-								<td>E-Mail</td>
-								<td>
-									<script>document.write(email);
-									</script>
-								</td>
-							</tr>
-	
-							<tr>
-								<td>Mesajınız</td>
-								<td>
-									<script>document.write(message);
-									</script>
-								</td>
-							</tr>
-	
-						</tbody>
-					
-				</table>	
-
-
-			</div>
+			</form>
 
 		</div>
 
 	</main>
 
 
-	<footer class="py-2 bg-dark text-white text-center fixed-bottom ">
+	<footer class="py-2 bg-dark text-light text-center fixed-bottom ">
 		Web-Teknolojileri-Projesi © Beytullah Yayla 2021
 	</footer>
 
-	
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
+	<!-- BOOTSTRAP -->
 	
 
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
 
 </body>
 
 </html>
+
